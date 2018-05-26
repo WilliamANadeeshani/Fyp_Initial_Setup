511c7f0339bfe77b29a85633a4a8c5286cb5a726
WilliamANadeeshani <nadeeshanijayarathna93@gmail.com>
2018-05-26 11:38:00 +0530

    initial-setup

---
+++ wiki_out_frequency_handle.py
@@ -0,0 +1,26 @@
+file_object = open('wiki_output', 'r')
+file = open('frequent_path', 'w')
+
+with open('wiki_output') as f:
+    content = f.readlines()
+
+content = [x.strip() for x in content]
+out = []
+read_path = []
+path_frequency = []
+
+for x in content:
+    line = x.split()
+    if line in read_path:
+        for x in path_frequency:
+            if x[0] == line[2]:
+                x[1] = x[1] + 1
+
+    else:
+        read_path.append(line)
+        temp = [line[2], 1]
+        path_frequency.append(temp)
+
+for x in path_frequency:
+    if x[1] >= 5:
+        file.write(str(x[0]) + "\n")
